bc4196ee18b127fe4d54b45c3b33a0c8aff79751
joshuabutler <butlerja.nhs@gmail.com>
2024-03-29 16:59:06 -0400

    edit styling on icons and correct link to email

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+// function findMedian(arr) {
+//     // Write your code here
+//     // divide the length of the array by 2
+//     let dividedLength = arr.length / 2
+//     // if the length divided is a whole number, that is the median
+//     if (dividedLength % 1 == 0) {
+//         console.log(arr[dividedLength])
+//     } else {
+//         console.log((arr[dividedLength] + (arr[dividedLength + 1])) / 2)
+//     }
+// }
